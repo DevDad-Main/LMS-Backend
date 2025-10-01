26db050fdc2f747dc16941901fbebf91f0454eb5
Oliver Metz <softwaredevdad@gmail.com>
2025-10-01 09:49:16 +0200

    add a check to middleware so we dont get the error mesage if a user is signed in and not an instructor

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -41,6 +41,10 @@ export const isAuthenticated = catchAsync(async (req, res, next) => {
 export const isInstructorAuthenticated = catchAsync(async (req, res, next) => {
   // Check if token exists in cookies
   const token = req.cookies.instructorToken;
+
+  if (!token && req.cookies.token)
+    return res.json({ message: "No Need To Proceed" });
+
   if (!token) {
     throw new AppError(
       "You are not logged in. Please log in to get access.",
