e1951fae0f956adfe12c77df7c085a3d329b2b65
Oliver Metz <softwaredevdad@gmail.com>
2025-10-19 20:30:34 +0200

    add mongodb test server to allow us to connect to our simulated mongoDB server, add hooks to run logic before and after. add user test suite an unit tests to test main functionality from pre hooks and virtuals

---
+++ tests/setup/mongodb.js
@@ -0,0 +1,24 @@
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  console.log("🧪 Connected to test MongoDB:", uri);
+  await mongoose.connect(uri);
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
---
+++ tests/user.test.js
@@ -0,0 +1,39 @@
+import { describe, expect, it } from "vitest";
+import bcrypt from "bcryptjs";
+import { User } from "../models/User.model.js";
+import "./setup/mongodb.js";
+
+//#region User model Test Suite
+describe("User model", () => {
+  it("should hash the password before saving", async () => {
+    const user = new User({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "password123",
+    });
+    await user.save();
+
+    expect(user.password).not.toBe("password123");
+
+    const isMatch = await bcrypt.compare("password123", user.password);
+    expect(isMatch).toBe(true);
+  });
+
+  it("should fail if required fields are missing", async () => {
+    const user = new User({});
+    await expect(user.save()).rejects.toThrowError();
+  });
+
+  it("should compare the passwords correctly", async () => {
+    const user = new User({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "password123",
+    });
+    await user.save();
+
+    const isMatch = await user.comparePassword("password123");
+    expect(isMatch).toBe(true);
+  });
+});
+//#endregion
